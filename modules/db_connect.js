4b84746c76b3d9577305368bad911b9ec7ef7495
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-08-29 15:48:27 +0800

    process.env

--- modules/db_connect.js
+++ modules/db_connect.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 //解構指定的.env檔
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 console.log("db", DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+console.log("process", process.env);
 
 //設定連線資訊
 const pool = mysql.createPool({
